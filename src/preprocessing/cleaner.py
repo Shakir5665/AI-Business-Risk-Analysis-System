be8e5a9146ca45a89835168f14998211ca6c5880
Mohamed shakir <shakirsar225974@gmail.com>
2026-06-25 16:02:59 +0530

    fix: comment out Unicode whitespace removal in TextCleaner

--- src/preprocessing/cleaner.py
+++ src/preprocessing/cleaner.py
@@ -129,7 +129,7 @@ class TextCleaner:
         # Remove Unicode whitespace
         # -------------------------------------------------
 
-        text = UNICODE_WHITESPACE_PATTERN.sub(" ", text)
+        # text = UNICODE_WHITESPACE_PATTERN.sub(" ", text)
 
         # -------------------------------------------------
         # Remove control characters
