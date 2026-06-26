76fe1ba8974bf088892d0dc1415aea5b99454994
Mohamed shakir <shakirsar225974@gmail.com>
2026-06-26 15:10:19 +0530

    feat: Implement Sentiment and Aspect Classification Heads with forward methods

---
+++ tests/test_classification_heads.py
@@ -0,0 +1,43 @@
+import torch
+
+from src.model.classification_heads import (
+    SentimentHead,
+    AspectHead
+)
+
+# -------------------------------------
+
+dummy_input = torch.randn(
+    32,
+    768
+)
+
+# -------------------------------------
+
+sentiment_head = SentimentHead()
+
+sentiment_output = sentiment_head(
+    dummy_input
+)
+
+print()
+
+print("Sentiment Output Shape")
+
+print(sentiment_output.shape)
+
+# -------------------------------------
+
+aspect_head = AspectHead(
+    num_aspect_classes=3
+)
+
+aspect_output = aspect_head(
+    dummy_input
+)
+
+print()
+
+print("Aspect Output Shape")
+
+print(aspect_output.shape)
